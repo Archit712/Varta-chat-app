70fddad073bf0f7706c928cbc5a6fbacad7b57a6
Archit Prasher <archit.prasher.22cse@bmu.edu.in>
2024-07-24 23:53:08 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import authRoute from "./routes/auth.routes.js";
 import messageRoute from "./routes/messageRoute.js";
@@ -11,6 +12,8 @@ import {app,server} from './socket/socket.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+
+const __dirname =path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 
@@ -38,6 +41,12 @@ app.get('/', (req, res) => {
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
 app.use("/api/users", userRoute);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
